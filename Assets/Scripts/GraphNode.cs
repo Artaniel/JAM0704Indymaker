09ffd5ffd9787b7d93493d895c31c459166676c2
Artaniel <59449523+Artaniel@users.noreply.github.com>
2023-04-08 23:42:34 +0300

    node moder and light scripting

--- Assets/Scripts/GraphNode.cs
+++ Assets/Scripts/GraphNode.cs
@@ -5,6 +5,12 @@ public class GraphNode : MonoBehaviour
 {
     public List<GraphNode> neighbours;
     public GameObject currentRobot = null;
+    public MeshRenderer lightRing;
+    public Material RMat;
+    public Material GMat;
+    public Material BMat;
+    public Material YMat;
+    public Material blankMat;
 
     public void RobotDragged(GameObject robot, Vector3 startPosOfNewRobot) {
         if (currentRobot == null)
@@ -33,9 +39,22 @@ public class GraphNode : MonoBehaviour
             newRobot.node = newNode;
             currentRobot = robot;
         }
+        RefreshLight();
     }
 
     public void RemoveRobot() {
         currentRobot = null;
+    }
+
+    private void RefreshLight() {
+        if (currentRobot)
+            switch (currentRobot.GetComponent<Robot>().robotype) {
+                case Robotype.R: lightRing.material = RMat; break;
+                case Robotype.G: lightRing.material = GMat; break;
+                case Robotype.B: lightRing.material = BMat; break;
+                case Robotype.Y: lightRing.material = YMat; break;
+            }
+        else
+            lightRing.material = blankMat;
     }
 }
