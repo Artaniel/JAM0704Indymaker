1dfbd7c8c6dbb42ad17b47aed487e190bf7543e4
YodezOri <95533992+YodezOri@users.noreply.github.com>
2023-04-10 21:51:39 +0300

    UISounds_02

--- Assets/Audio/Scripts/AudioUi.cs
+++ Assets/Audio/Scripts/AudioUi.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioUi : MonoBehaviour
 {
-   
-    //создать пустой объект "UI" в "MainAudio", закинуть на него этот скрипт
+    public Button buttonComp;
+    private bool haveButton;
+    private bool activity;
+    
+
+    private void OnValidate()
+    {
+        if (gameObject.GetComponent<Button>() == null)
+        {
+            haveButton = false;
+        }
+        else
+        {
+            haveButton = true;
+        }
+    }
+    private void Start()
+    {
+        if (haveButton)
+        {
+            buttonComp = GetComponent<Button>();
+        }
+    }
+    private void Update()
+    {
+        if (haveButton)
+        {
+            activity = buttonComp.interactable;
+        }
+    }
     public void MouseOnSound() // курсор на кнопке/слайдере
     {
         FMODUnity.RuntimeManager.PlayOneShotAttached("event:/UI/MenuUI/UI_MouseOn", gameObject);
     }
     public void MouseClickSound() // клик по кнопке/слайдеру
     {
-        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/UI/MenuUI/UI_Select", gameObject);
+        if (haveButton)
+        {
+            if (activity)
+            {
+                FMODUnity.RuntimeManager.PlayOneShotAttached("event:/UI/MenuUI/UI_Select", gameObject);
+                Debug.Log("button active");
+            }
+            else
+            {
+                FMODUnity.RuntimeManager.PlayOneShotAttached("event:/UI/MenuUI/UI_Disabled", gameObject);
+                Debug.Log("button disabled");
+            }
+        }
+        else
+        {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/UI/MenuUI/UI_Select", gameObject);
+            Debug.Log("nobutton");
+        }
     }
     public void MouseDeClickSound() // клик по кнопке/слайдеру
     {
@@ -26,5 +72,7 @@ public class AudioUi : MonoBehaviour
     {
         FMODUnity.RuntimeManager.StudioSystem.setParameterByName("parameter:/RTPS_Menu", 1);
     }
+
+
 
 }
